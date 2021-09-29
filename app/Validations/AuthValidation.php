49a8b1c43aa64484949aaf523b3eccfe6a1f6a32
Danil <kostynd1@gmail.com>
2021-09-29 22:55:07 +0300

    Added validation input data for auth

---
+++ app/Validations/AuthValidation.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Validations;
+
+use Codememory\Components\Translator\Interfaces\TranslationInterface;
+use Codememory\Components\Validator\Interfaces\ValidateInterface;
+use Codememory\Components\Validator\Interfaces\ValidationBuildInterface;
+use Codememory\Components\Validator\Interfaces\ValidatorInterface;
+
+/**
+ * Class AuthValidation
+ *
+ * @package App\Validations
+ *
+ * @author  Danil
+ */
+class AuthValidation implements ValidationBuildInterface
+{
+
+    /**
+     * @inheritDoc
+     */
+    public function build(ValidatorInterface $validator, ...$args): void
+    {
+
+        /** @var TranslationInterface $translation */
+        $translation = $args['translation'];
+
+        $validator
+            ->addValidation('username', function (ValidateInterface $validate) use ($translation) {
+                $validate->addRule('required')->addMessage($translation->getTranslationActiveLang('auth.requiredUsername'));
+            })
+            ->addValidation('password', function (ValidateInterface $validate) use ($translation) {
+                $validate->addRule('required')->addMessage($translation->getTranslationActiveLang('auth.requiredPassword'));
+            });
+
+    }
+
+}
