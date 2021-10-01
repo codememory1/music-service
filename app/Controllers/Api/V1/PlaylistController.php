cc192b917b80f3f30de4cc019ea962c0137d1291
Danil <kostynd1@gmail.com>
2021-10-01 15:12:33 +0300

    Added method to controller for output data playlist

--- app/Controllers/Api/V1/PlaylistController.php
+++ app/Controllers/Api/V1/PlaylistController.php
@@ -8,7 +8,9 @@ use App\Services\Playlist\PlaylistCreatorService;
 use Codememory\Components\Database\QueryBuilder\Exceptions\NotSelectedStatementException;
 use Codememory\Components\Database\QueryBuilder\Exceptions\QueryNotGeneratedException;
 use Codememory\Components\DateTime\Exceptions\InvalidTimezoneException;
+use Codememory\Components\Profiling\Exceptions\BuilderNotCurrentSectionException;
 use Codememory\Components\Services\Exceptions\ServiceNotExistException;
+use Codememory\Container\ServiceProvider\Interfaces\ServiceProviderInterface;
 use ReflectionException;
 
 /**
@@ -20,6 +22,29 @@ use ReflectionException;
  */
 class PlaylistController extends AbstractAuthorizationController
 {
+
+    /**
+     * @var PlaylistRepository
+     */
+    private PlaylistRepository $playlistRepository;
+
+    /**
+     * @param ServiceProviderInterface $serviceProvider
+     *
+     * @throws ReflectionException
+     * @throws ServiceNotExistException
+     * @throws BuilderNotCurrentSectionException
+     */
+    public function __construct(ServiceProviderInterface $serviceProvider)
+    {
+
+        parent::__construct($serviceProvider);
+
+        /** @var PlaylistRepository $playlistRepository */
+        $playlistRepository = $this->getDatabase()->getEntityManager()->getRepository(PlaylistEntity::class);
+        $this->playlistRepository = $playlistRepository;
+
+    }
 
     /**
      * @throws NotSelectedStatementException
@@ -35,6 +60,25 @@ class PlaylistController extends AbstractAuthorizationController
 
             $this->response->json($playlistRepository->findAllAsArray([
                 'userid' => $authorizedUser->getUserid()
+            ]));
+        }
+
+    }
+
+    /**
+     * @param int $id
+     *
+     * @throws NotSelectedStatementException
+     * @throws QueryNotGeneratedException
+     * @throws ReflectionException
+     */
+    public function show(int $id): void
+    {
+
+        if (false != $authorizedUser = $this->isAuthWithResponse()) {
+            $this->response->json($this->playlistRepository->findOneAsArray([
+                'userid' => $authorizedUser->getUserid(),
+                'id'     => $id
             ]));
         }
 
