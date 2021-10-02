3c7df44d09038c92c7df5ee058f15e4c588ce742
Danil <kostynd1@gmail.com>
2021-10-02 21:25:45 +0300

    Added validation for update playlist

---
+++ app/Validations/PlaylistUpdateValidation.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Validations;
+
+/**
+ * Class PlaylistUpdateValidation
+ *
+ * @package App\Validations
+ *
+ * @author  Danil
+ */
+class PlaylistUpdateValidation extends PlaylistCreationValidation
+{
+
+}
