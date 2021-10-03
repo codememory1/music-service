b69a76f7cf311f9df2573bc5e48b99bee0820c09
Danil <kostynd1@gmail.com>
2021-10-04 00:57:31 +0300

    Optimization user repository

--- app/Orm/Repositories/UserRepository.php
+++ app/Orm/Repositories/UserRepository.php
@@ -6,7 +6,6 @@ use App\Orm\Entities\UserEntity;
 use Codememory\Components\Database\Orm\Repository\AbstractEntityRepository;
 use Codememory\Components\Database\QueryBuilder\Exceptions\NotSelectedStatementException;
 use Codememory\Components\Database\QueryBuilder\Exceptions\QueryNotGeneratedException;
-use Codememory\Components\Database\QueryBuilder\Interfaces\QueryBuilderInterface;
 use ReflectionException;
 
 /**
@@ -30,14 +29,7 @@ class UserRepository extends AbstractEntityRepository
     public function findOne(array $by): bool|UserEntity
     {
 
-        $qb = $this->createQueryBuilder();
-        $qb
-            ->setParameters($by)
-            ->select()
-            ->from($this->getEntityData()->getTableName())
-            ->where($qb->expression()->exprAnd(...$this->getConditionsFromBy($qb, $by)));
-
-        $result = $qb->generateQuery()->toEntity();
+        $result = $this->findBy($by)->toEntity();
 
         return [] !== $result ? $result[0] : false;
 
@@ -54,81 +46,25 @@ class UserRepository extends AbstractEntityRepository
     public function findOneByOr(array $by): bool|UserEntity
     {
 
-        $qb = $this->createQueryBuilder();
-        $qb
-            ->setParameters($by)
-            ->select()
-            ->from($this->getEntityData()->getTableName())
-            ->where($qb->expression()->exprOr(...$this->getConditionsFromBy($qb, $by)));
-
-        $result = $qb->generateQuery()->toEntity();
+        $result = $this->findBy($by, 'or')->toEntity();
 
         return [] !== $result ? $result[0] : false;
 
     }
 
     /**
-     * @return int
-     * @throws NotSelectedStatementException
-     * @throws QueryNotGeneratedException
-     */
-    public function getCountUsers(): int
-    {
-
-        $qb = $this->createQueryBuilder();
-
-        $qb->customSelect()->columns(['count' => 'COUNT(*)'])->from('users');
-
-        return $qb->generateQuery()->getResult()->toArray()[0]['count'];
-    }
-
-    /**
      * @param array  $data
-     * @param string $byEmail
+     * @param string $email
      *
      * @return void
-     * @throws QueryNotGeneratedException
      * @throws NotSelectedStatementException
+     * @throws QueryNotGeneratedException
+     * @throws ReflectionException
      */
-    public function update(array $data, string $byEmail): void
+    public function updateUser(array $data, string $email): void
     {
 
-        $qb = $this->createQueryBuilder();
-        $values = array_map(function (mixed $column) {
-            return ':' . $column;
-        }, array_flip($data));
-
-        $qb
-            ->setParameters($data)
-            ->setParameter('where_email', $byEmail)
-            ->update($this->getEntityData()->getTableName())
-            ->setData(array_keys($data), $values)
-            ->where(
-                $qb->expression()->exprAnd(
-                    $qb->expression()->condition('email', '=', ':where_email')
-                )
-            );
-
-        $qb->generateQuery()->execute();
-
-    }
-
-    /**
-     * @param QueryBuilderInterface $queryBuilder
-     * @param array                 $by
-     *
-     * @return array
-     */
-    private function getConditionsFromBy(QueryBuilderInterface $queryBuilder, array $by): array
-    {
-
-        $conditions = [];
-
-        foreach ($by as $column => $value) {
-            $conditions[] = $queryBuilder->expression()->condition($column, '=', ':' . $column);
-        }
-
-        return $conditions;
+        $this->update($data, ['email' => $email]);
 
     }
 
