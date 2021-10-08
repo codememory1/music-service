8994b029efbc2a95989af33f08c439f6b7b4c66c
Danil <kostynd1@gmail.com>
2021-10-08 15:03:49 +0300

    Insert default roles

---
+++ migrations/Migration1633520483InsertRoles.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Migrations;
+
+use App\Orm\Repositories\RoleRepository;
+use Codememory\Components\Database\Migrations\AbstractMigration;
+use Codememory\Components\Database\Migrations\Interfaces\SchemaInterface as MigrationSchemaInterface;
+
+/**
+ * Class Migration1633520483InsertRoles
+ *
+ * @package Migrations
+ */
+final class Migration1633520483InsertRoles extends AbstractMigration
+{
+
+    /**
+     * @inheritDoc
+     */
+    public function up(MigrationSchemaInterface $schema): void
+    {
+
+        $schema->selectTable('roles');
+
+        $schema->insertRecords(['id', 'name'], ...[
+            [1, RoleRepository::USER_ROLE],
+            [2, RoleRepository::ADMIN_ROLE],
+            [3, RoleRepository::DEV_ROLE],
+            [4, RoleRepository::MUSIC_MANAGER_ROLE],
+            [5, RoleRepository::SUPPORT_ROLE]
+        ]);
+
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function down(MigrationSchemaInterface $schema): void
+    {
+
+        $schema->selectTable('roles');
+
+        $schema->deleteRecords()->where(
+            $schema->expression()->exprAnd(
+                $schema->expression()->condition('id', '=', 1),
+                $schema->expression()->condition('id', '=', 2),
+                $schema->expression()->condition('id', '=', 3),
+                $schema->expression()->condition('id', '=', 4),
+                $schema->expression()->condition('id', '=', 5)
+            )
+        );
+
+    }
+
+}
