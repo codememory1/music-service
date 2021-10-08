e24a2ba8f882a64eb9479db211d5ad20a2e2ffc0
Danil <kostynd1@gmail.com>
2021-10-08 14:53:45 +0300

    Added enum for statuses

---
+++ app/Orm/Repositories/Enums/StatusEnum.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Orm\Repositories\Enums;
+
+/**
+ * Class StatusEnum
+ *
+ * @package App\Orm\Repositories\Enums
+ *
+ * @author  Danil
+ */
+final class StatusEnum
+{
+
+    public const NOT_ACTIVATED = 0;
+    public const ACTIVATED = 1;
+    public const BLOCKED = 2;
+
+}
