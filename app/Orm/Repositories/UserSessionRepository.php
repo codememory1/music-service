e1d20da7023d653c10f7053feb3fa06d439d55cc
Danil <kostynd1@gmail.com>
2021-10-09 12:17:54 +0300

    Added a method for removing invalid tokens

--- app/Orm/Repositories/UserSessionRepository.php
+++ app/Orm/Repositories/UserSessionRepository.php
@@ -3,15 +3,41 @@
 namespace App\Orm\Repositories;
 
 use Codememory\Components\Database\Orm\Repository\AbstractEntityRepository;
+use Codememory\Components\Database\QueryBuilder\Exceptions\NotSelectedStatementException;
+use Codememory\Components\Database\QueryBuilder\Exceptions\QueryNotGeneratedException;
+use ReflectionException;
 
 /**
  * Class UserSessionRepository
  *
  * @package App\Orm\Repositories
+ *
+ * @author  Danil
  */
 class UserSessionRepository extends AbstractEntityRepository
 {
 
+    /**
+     * @return void
+     * @throws NotSelectedStatementException
+     * @throws QueryNotGeneratedException
+     * @throws ReflectionException
+     */
+    public function deleteInvalidTokens(): void
+    {
 
+        $qb = $this->createQueryBuilder();
+
+        $qb
+            ->delete($this->getEntityData()->getTableName())
+            ->where(
+                $qb->expression()->exprAnd(
+                    $qb->expression()->condition('valid_to', '<', 'NOW()')
+                )
+            );
+
+        $qb->generateQuery()->execute();
+
+    }
 
 }
