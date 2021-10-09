ecb107222a5a96d365ec3c255b800c7055bff428
Danil <kostynd1@gmail.com>
2021-10-09 12:19:30 +0300

    Added service for saving user session

---
+++ app/Services/Auth/SaveSessionService.php
@@ -0,0 +1,123 @@
+<?php
+
+namespace App\Services\Auth;
+
+use App\Orm\Entities\UserEntity;
+use App\Orm\Entities\UserSessionEntity;
+use App\Orm\Repositories\UserSessionRepository;
+use App\Services\AbstractApiService;
+use App\Services\Tokens\SessionTokenService;
+use Codememory\Components\Database\Orm\Interfaces\EntityManagerInterface;
+use Codememory\Components\Database\QueryBuilder\Exceptions\NotSelectedStatementException;
+use Codememory\Components\Database\QueryBuilder\Exceptions\QueryNotGeneratedException;
+use Codememory\Components\GEO\Geolocation;
+use ReflectionException;
+
+/**
+ * Class SaveSessionService
+ *
+ * @package App\Services\Auth
+ *
+ * @author  Danil
+ */
+class SaveSessionService extends AbstractApiService
+{
+
+    /**
+     * @param EntityManagerInterface $entityManager
+     * @param UserEntity             $userEntity
+     * @param string                 $refreshToken
+     *
+     * @return void
+     * @throws NotSelectedStatementException
+     * @throws QueryNotGeneratedException
+     * @throws ReflectionException
+     */
+    public function save(EntityManagerInterface $entityManager, UserEntity $userEntity, string $refreshToken): void
+    {
+
+        /** @var UserSessionRepository $userSessionRepository */
+        $userSessionRepository = $entityManager->getRepository(UserSessionEntity::class);
+
+        // Collecting UserSessionEntity with basic information
+        $userSessionEntity = $this->getCollectedUserSessionEntity($userEntity, $refreshToken);
+
+        // Add IP information to UserSessionEntity, if information exists for this IP
+        $userSessionEntity = $this->addIpInfo($userSessionEntity);
+
+        // We delete records from the table where the token lifetime has already expired
+        $this->removeInvalidTokens($userSessionRepository);
+
+        // We save information about authorization to the table
+        $entityManager->commit($userSessionEntity)->flush();
+
+    }
+
+    /**
+     * @param UserEntity $userEntity
+     * @param string     $refreshToken
+     *
+     * @return UserSessionEntity
+     */
+    private function getCollectedUserSessionEntity(UserEntity $userEntity, string $refreshToken): UserSessionEntity
+    {
+
+        /** @var SessionTokenService $sessionToken */
+        $sessionToken = $this->get('session-token');
+        $refreshTokenValidTo = date('Y-m-d H:i:s', $sessionToken->decodeRefresh($refreshToken)->exp);
+
+        $userSessionEntity = new UserSessionEntity();
+        $userSessionEntity
+            ->setUserId($userEntity->getId())
+            ->setRefreshToken($refreshToken)
+            ->setIp($this->request->getIp())
+            ->setValidTo($refreshTokenValidTo);
+
+        return $userSessionEntity;
+
+    }
+
+    /**
+     * @param UserSessionEntity $userSessionEntity
+     *
+     * @return UserSessionEntity
+     */
+    private function addIpInfo(UserSessionEntity $userSessionEntity): UserSessionEntity
+    {
+
+        $geolocation = (new Geolocation())->setIp($this->request->getIp());
+
+        // We set information about the ip address, if there is information for this ip
+        if ($geolocation->isSuccess()) {
+            $location = $geolocation->getLocation();
+            $country = $location->getCountry();
+
+            $userSessionEntity
+                ->setCountry($country->getName())
+                ->setCodeCountry($country->getCode())
+                ->setRegion($location->getRegion()->getName())
+                ->setCity($location->getCity()->getName())
+                ->setLatitude($country->getLatitude())
+                ->setLongitude($country->getLongitude());
+        }
+
+        return $userSessionEntity;
+
+    }
+
+    /**
+     * @param UserSessionRepository $userSessionRepository
+     *
+     * @return void
+     * @throws NotSelectedStatementException
+     * @throws QueryNotGeneratedException
+     * @throws ReflectionException
+     */
+    private function removeInvalidTokens(UserSessionRepository $userSessionRepository): void
+    {
+
+        $userSessionRepository->deleteInvalidTokens();
+
+    }
+
+}
