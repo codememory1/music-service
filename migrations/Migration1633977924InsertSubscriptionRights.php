959cd16b5fe784aae81a9ae3bee17f494c6d40a5
Danil <kostynd1@gmail.com>
2021-10-11 23:08:33 +0300

    Created migrations for adding new rights to roles

---
+++ migrations/Migration1633977924InsertSubscriptionRights.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Migrations;
+
+use Codememory\Components\Database\Migrations\AbstractMigration;
+use Codememory\Components\Database\Migrations\Interfaces\SchemaInterface as MigrationSchemaInterface;
+
+/**
+ * Class Migration1633977924InsertSubscriptionRights
+ *
+ * @package Migrations
+ */
+final class Migration1633977924InsertSubscriptionRights extends AbstractMigration
+{
+
+    /**
+     * @inheritDoc
+     */
+    public function up(MigrationSchemaInterface $schema): void
+    {
+
+        $schema->selectTable('role_rights');
+
+        $schema->insertRecords(['access_right', 'role_id'], ...[
+            # Administrator
+            [11, 2], [12, 2], [13, 2],
+
+            # Developer
+            [11, 3], [12, 3], [13, 3]
+        ]);
+
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function down(MigrationSchemaInterface $schema): void
+    {
+
+    
+
+    }
+
+}
