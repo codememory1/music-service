60515f42d8476ad2e8aa8c7510eda7b88ed9b7b8
Danil <kostynd1@gmail.com>
2021-10-11 23:11:27 +0300

    A service for deleting a subscription has been created

---
+++ app/Services/Subscription/RemoverService.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Services\Subscription;
+
+use App\Orm\Repositories\SubscriptionRepository;
+use App\Services\AbstractApiService;
+use App\Services\ResponseApiCollectorService;
+use Codememory\Components\Database\QueryBuilder\Exceptions\NotSelectedStatementException;
+use Codememory\Components\Database\QueryBuilder\Exceptions\QueryNotGeneratedException;
+use ReflectionException;
+
+/**
+ * Class RemoverService
+ *
+ * @package App\Services\Subscription
+ *
+ * @author  Danil
+ */
+class RemoverService extends AbstractApiService
+{
+
+    /**
+     * @param SubscriptionRepository $subscriptionRepository
+     * @param int                    $id
+     *
+     * @return ResponseApiCollectorService
+     * @throws NotSelectedStatementException
+     * @throws QueryNotGeneratedException
+     * @throws ReflectionException
+     */
+    final public function delete(SubscriptionRepository $subscriptionRepository, int $id): ResponseApiCollectorService
+    {
+
+        $subscription = $subscriptionRepository->findOne(['id' => $id]);
+
+        // Checking the existence of a subscription
+        if (false === $subscription) {
+            return $this->createApiResponse(404, 'subscription.subscriptionNotExist');
+        }
+
+        // Subscription found. Removing the subscription itself and its options
+        $subscriptionRepository->deleteById($id);
+
+        return $this->createApiResponse(200, 'subscription.successDelete');
+
+    }
+
+}
