36e0817e56cfc3c2c6b66b144c42dd6341fb340a
Danil <kostynd1@gmail.com>
2021-10-11 03:18:49 +0300

    Compressing routes

--- routes/api.php
+++ routes/api.php
@@ -15,30 +15,10 @@ use Codememory\Routing\Router;
  */
 
 Router::subdomainGroup('api', function () {
-    Router::post('register/', RegisterController::class . '#register')->name('register');
-    Router::get('register/activate/:token/', RegisterController::class . '#activation')
-        ->with('token', '.+')
-        ->name('account-activation');
-    Router::post('auth/', AuthController::class . '#auth')->name('auth');
+    Router::post('/register', RegisterController::class . '#register');
+    Router::get('/register/activate/:token', RegisterController::class . '#activation')->with('token', '.+');
+    Router::post('/auth', AuthController::class . '#auth');
 
-    // Playlist routes
-    Router::group('playlist/', function () {
-        Router::post('create/', PlaylistController::class . '#create')->name('create-playlist');
-        Router::get('all/', PlaylistController::class . '#all')->name('all-playlists');
-        Router::get('show/:id', PlaylistController::class . '#show')
-            ->with('id', '[0-9]+')
-            ->name('show-playlist');
-        Router::put('update/:id', PlaylistController::class . '#update')
-            ->with('id', '[0-9]+')
-            ->name('update-playlist');
-        Router::delete('delete/:id', PlaylistController::class . '#delete')
-            ->with('id', '[0-9]+')
-            ->name('delete-playlist');
-    });
-
-    // Subscription routes
-    Router::group('subscription/', function () {
-        Router::get('all/', SubscriptionController::class . '#all')->name('all-subscriptions');
-        Router::post('create/', SubscriptionController::class . '#create')->name('create-subscriptions');
-    });
+    Router::resource('/playlist', PlaylistController::class);
+    Router::resource('/subscription', SubscriptionController::class);
 });
