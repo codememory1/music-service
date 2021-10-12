d396eca64868ababc3a731725c99a208732e2329
Danil <kostynd1@gmail.com>
2021-10-12 21:06:58 +0300

    Created validation of subscription update input

---
+++ app/Validations/Subscription/SubscriptionUpdatingValidation.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Validations\Subscription;
+
+/**
+ * Class SubscriptionUpdatingValidation
+ *
+ * @package App\Validations\Subscription
+ *
+ * @author  Danil
+ */
+class SubscriptionUpdatingValidation extends SubscriptionCreationValidation
+{
+
+}
