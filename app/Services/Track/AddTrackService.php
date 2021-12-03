dea2afea1fa6931ec00af306e478ac42749a4e5d
Danil <kostynd1@gmail.com>
2021-12-03 13:00:08 +0200

    Сервис добавления трека

--- app/Services/Track/AddTrackService.php
+++ app/Services/Track/AddTrackService.php
@@ -3,6 +3,10 @@
 namespace App\Services\Track;
 
 use App\Services\AbstractApiService;
+use App\Validations\Track\TrackAddValidation;
+use Codememory\Components\Database\Orm\Interfaces\EntityManagerInterface;
+use Codememory\Components\Validator\Interfaces\ValidationManagerInterface;
+use Codememory\Components\Validator\Manager as ValidationManager;
 
 /**
  * Class AddTrackService
@@ -14,10 +18,31 @@ use App\Services\AbstractApiService;
 class AddTrackService extends AbstractApiService
 {
 
-    final public function make()
+    final public function make(ValidationManager $validationManager, EntityManagerInterface $entityManager)
     {
 
+        $inputValidation = $this->inputValidation($validationManager);
 
+        // Input data validation
+        if(!$inputValidation->isValidation()) {
+            return $this->apiResponse->create(400, $inputValidation->getErrors());
+        }
+
+        // Checking for the existence of a track category
+
+        // Checking if an album exists
+
+    }
+
+    /**
+     * @param ValidationManager $validationManager
+     *
+     * @return ValidationManagerInterface
+     */
+    private function inputValidation(ValidationManager $validationManager): ValidationManagerInterface
+    {
+
+        return $validationManager->create(new TrackAddValidation(), $this->request->post()->all());
 
     }
 
