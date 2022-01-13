6d02dd41f0b27383a4bb3cb042e9f8ebc14bb762
Danil <kostynd1@gmail.com>
2022-01-13 20:01:49 +0200

    Выполнение сервиса creator

--- app/Controllers/V1/AlbumController.php
+++ app/Controllers/V1/AlbumController.php
@@ -2,7 +2,13 @@
 
 namespace App\Controllers\V1;
 
+use App\Orm\Repositories\AccessRightNameRepository;
+use App\Services\Album\CreatorService;
+use Codememory\Components\Database\QueryBuilder\Exceptions\StatementNotSelectedException;
+use Codememory\Components\Services\Exceptions\ServiceNotExistException;
+use ErrorException;
 use JetBrains\PhpStorm\NoReturn;
+use ReflectionException;
 
 /**
  * Class AlbumController
@@ -11,14 +17,31 @@ use JetBrains\PhpStorm\NoReturn;
  *
  * @author  Danil
  */
-class AlbumController extends AbstractApiController
+class AlbumController extends AbstractAuthorizationController
 {
 
+    /**
+     * @return void
+     * @throws StatementNotSelectedException
+     * @throws ServiceNotExistException
+     * @throws ErrorException
+     * @throws ReflectionException
+     */
     #[NoReturn]
     public function create(): void
     {
 
-        
+        $authorizedUser = $this->checkAuthWithRight(AccessRightNameRepository::ADD_MUSIC);
+
+        /** @var CreatorService $albumCreatorService */
+        $albumCreatorService = $this->getService('Album\Creator');
+
+        // Album creation response
+        $createResponse = $albumCreatorService
+            ->make($this->validatorManager(), $authorizedUser)
+            ->getResponseApiCollector();
+
+        $this->response->json($createResponse->getResponse(), $createResponse->getStatus());
 
     }
 
