acc6cd9de23f619f1811bb046a4b79d00a42f3dd
Danil <kostynd1@gmail.com>
2022-01-13 19:51:04 +0200

    Создан контроллер и метод создания альбома

---
+++ app/Controllers/V1/AlbumController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controllers\V1;
+
+use JetBrains\PhpStorm\NoReturn;
+
+/**
+ * Class AlbumController
+ *
+ * @package App\Controllers\V1
+ *
+ * @author  Danil
+ */
+class AlbumController extends AbstractApiController
+{
+
+    #[NoReturn]
+    public function create(): void
+    {
+
+        
+
+    }
+
+}
