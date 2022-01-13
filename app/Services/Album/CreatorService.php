b291dc6783498bc68c19c23082d4d50b8f603498
Danil <kostynd1@gmail.com>
2022-01-13 19:57:22 +0200

    Создан сервис создания альбома

---
+++ app/Services/Album/CreatorService.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Services\Album;
+
+use App\Services\AbstractCrudService;
+
+/**
+ * Class CreatorService
+ *
+ * @package App\Services\Album
+ *
+ * @author  Danil
+ */
+class CreatorService extends AbstractCrudService
+{
+
+}
