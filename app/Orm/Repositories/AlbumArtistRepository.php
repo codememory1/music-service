a16541ab13efd8c8e7920ecc0393aa19329b6335
Danil <kostynd1@gmail.com>
2022-01-14 16:46:54 +0200

    Создан метод получения артиста альбома

--- app/Orm/Repositories/AlbumArtistRepository.php
+++ app/Orm/Repositories/AlbumArtistRepository.php
@@ -2,7 +2,10 @@
 
 namespace App\Orm\Repositories;
 
+use App\Orm\Entities\AlbumArtistEntity;
 use Codememory\Components\Database\Orm\Repository\AbstractEntityRepository;
+use Codememory\Components\Database\QueryBuilder\Exceptions\StatementNotSelectedException;
+use ReflectionException;
 
 /**
  * Class AlbumArtistRepository
@@ -12,6 +15,22 @@ use Codememory\Components\Database\Orm\Repository\AbstractEntityRepository;
 class AlbumArtistRepository extends AbstractEntityRepository
 {
 
+    /**
+     * @param int $albumId
+     * @param int $userId
+     *
+     * @return AlbumArtistEntity|bool
+     * @throws StatementNotSelectedException
+     * @throws ReflectionException
+     */
+    public function findAsEntity(int $albumId, int $userId): AlbumArtistEntity|bool
+    {
 
+        return $this->customFindBy([
+            'album_id' => $albumId,
+            'user_id' => $userId
+        ])->entity()->first();
+
+    }
 
 }
