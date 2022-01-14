9990409d32400dc3beb93366799ceee02d18104d
Danil <kostynd1@gmail.com>
2022-01-14 16:47:54 +0200

    Описан сервис удаления

--- app/Services/Album/DeleterService.php
+++ app/Services/Album/DeleterService.php
@@ -2,8 +2,14 @@
 
 namespace App\Services\Album;
 
+use App\Orm\Entities\AlbumArtistEntity;
+use App\Orm\Entities\UserEntity;
+use App\Orm\Repositories\AlbumArtistRepository;
 use App\Orm\Repositories\AlbumRepository;
 use App\Services\AbstractCrudService;
+use Codememory\Components\Database\QueryBuilder\Exceptions\StatementNotSelectedException;
+use Codememory\Components\Services\Exceptions\ServiceNotExistException;
+use ReflectionException;
 
 /**
  * Class DeleterService
@@ -15,8 +21,54 @@ use App\Services\AbstractCrudService;
 class DeleterService extends AbstractCrudService
 {
 
-    public function make(AlbumRepository $albumRepository, int $id): static
+    /**
+     * @param UserEntity      $userEntity
+     * @param AlbumRepository $albumRepository
+     * @param int             $id
+     *
+     * @return $this
+     * @throws ReflectionException
+     * @throws StatementNotSelectedException
+     * @throws ServiceNotExistException
+     */
+    public function make(UserEntity $userEntity, AlbumRepository $albumRepository, int $id): static
     {
+
+        /** @var AlbumArtistRepository $albumArtistRepository */
+        $albumArtistRepository = $this->getRepository(AlbumArtistEntity::class);
+
+        $finedAlbumArtist = $albumArtistRepository->findAsEntity($id, $userEntity->getId());
+
+        // Checking if an album exists
+        if (!$finedAlbumArtist) {
+            $this->setResponse(
+                $this->createApiResponse(404, 'album@notExist')
+            );
+        }
+
+        // Deleting an album
+        $this->push($albumRepository, $albumArtistRepository, $finedAlbumArtist);
+
+        return $this->setResponse(
+            $this->createApiResponse(200, 'album@successDelete')
+        );
+
+    }
+
+    /**
+     * @param AlbumRepository       $albumRepository
+     * @param AlbumArtistRepository $albumArtistRepository
+     * @param AlbumArtistEntity     $albumArtistEntity
+     *
+     * @return void
+     * @throws StatementNotSelectedException
+     * @throws ReflectionException
+     */
+    private function push(AlbumRepository $albumRepository, AlbumArtistRepository $albumArtistRepository, AlbumArtistEntity $albumArtistEntity): void
+    {
+
+        $albumRepository->delete(['id' => $albumArtistEntity->getAlbumId()]);
+        $albumArtistRepository->delete(['id' => $albumArtistEntity->getAlbumId()]);
 
     }
 
