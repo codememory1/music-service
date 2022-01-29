262b66c50360e25a72db1a8ba45f488c40de529e
Danil <kostynd1@gmail.com>
2022-01-30 00:56:30 +0200

    Внесены правки в абстрактный контроллер

--- app/src/Controller/Api/AbstractApiController.php
+++ app/src/Controller/Api/AbstractApiController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller\Api;
 
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 
 /**
  * Class AbstractApiController
@@ -14,5 +16,25 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 abstract class AbstractApiController extends AbstractController
 {
 
+    /**
+     * @var Response
+     */
+    protected Response $response;
+
+    /**
+     * @var ManagerRegistry
+     */
+    protected ManagerRegistry $managerRegistry;
+
+    /**
+     * @param ManagerRegistry $managerRegistry
+     */
+    public function __construct(ManagerRegistry $managerRegistry)
+    {
+
+        $this->response = new Response();
+        $this->managerRegistry = $managerRegistry;
+
+    }
 
 }
