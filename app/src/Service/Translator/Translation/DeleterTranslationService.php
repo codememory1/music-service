4f55e64ca145265f632ae423f239d26864eea7f3
Danil <kostynd1@gmail.com>
2022-01-30 03:09:21 +0200

    Сервис удаления перевода

---
+++ app/src/Service/Translator/Translation/DeleterTranslationService.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Service\Translator\Translation;
+
+use App\Entity\Translation;
+use App\Enums\ApiResponseTypeEnum;
+use App\Repository\TranslationRepository;
+use App\Service\AbstractApiService;
+use App\Service\Response\ApiResponseService;
+use Exception;
+
+/**
+ * Class DeleterTranslationService
+ *
+ * @package App\Service\Translator\Translation
+ *
+ * @author  Codememory
+ */
+class DeleterTranslationService extends AbstractApiService
+{
+
+    /**
+     * @param int $id
+     *
+     * @return ApiResponseService
+     * @throws Exception
+     */
+    public function delete(int $id): ApiResponseService
+    {
+
+        /** @var TranslationRepository $translationRepository */
+        $translationRepository = $this->em->getRepository(Translation::class);
+
+        // Check exist language
+        if (null === $finedTranslation = $translationRepository->findOneBy(['id' => $id])) {
+            $this
+                ->prepareApiResponse('error', 404)
+                ->setMessage(
+                    ApiResponseTypeEnum::CHECK_EXIST,
+                    'translation_not_exist',
+                    $this->getTranslation('translation@notExist')
+                );
+
+            return $this->getPreparedApiResponse();
+        }
+
+        // Deleting a found entry
+        return $this->remove($finedTranslation, 'translation@successDelete');
+
+    }
+
+}
