27cca09ab92465357e9a6115f715746a2c131f1f
Danil <kostynd1@gmail.com>
2022-02-01 22:28:38 +0200

    Маршрут регистрации юзера и вызов сервиса регистрации

--- app/src/Controller/Api/V1/SecurityController.php
+++ app/src/Controller/Api/V1/SecurityController.php
@@ -3,9 +3,13 @@
 namespace App\Controller\Api\V1;
 
 use App\Controller\Api\AbstractApiController;
+use App\Service\Security\UserRegistrationService;
+use Exception;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\Messenger\MessageBusInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 /**
  * Class SecurityController
@@ -17,10 +21,21 @@ use Symfony\Component\Routing\Annotation\Route;
 class SecurityController extends AbstractApiController
 {
 
+    /**
+     * @param Request                  $request
+     * @param ValidatorInterface       $validator
+     * @param EventDispatcherInterface $eventDispatcher
+     *
+     * @return JsonResponse
+     * @throws Exception
+     */
     #[Route('/register', methods: 'POST')]
-    public function register(MessageBusInterface $bus): JsonResponse
+    public function register(Request $request, ValidatorInterface $validator, EventDispatcherInterface $eventDispatcher): JsonResponse
     {
 
+        $userRegistrationService = new UserRegistrationService($request, $this->response, $this->managerRegistry);
+
+        return $userRegistrationService->register($validator, $eventDispatcher)->make();
 
     }
 
