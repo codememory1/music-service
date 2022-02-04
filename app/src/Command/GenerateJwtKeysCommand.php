878d823dde7d711037db14203521a4b1bb8ba32c
Danil <kostynd1@gmail.com>
2022-02-04 16:17:51 +0200

    Изменена команда генерации токенов

--- app/src/Command/GenerateJwtKeysCommand.php
+++ app/src/Command/GenerateJwtKeysCommand.php
@@ -4,7 +4,9 @@ namespace App\Command;
 
 use sixlive\DotenvEditor\DotenvEditor;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
@@ -47,6 +49,15 @@ class GenerateJwtKeysCommand extends Command
 
     }
 
+    protected function configure(): void
+    {
+
+        $this
+            ->addArgument('variable-prefix', InputArgument::REQUIRED, 'Variable prefix. Example: JWT_ACCESS')
+            ->addOption('env-file', null, InputOption::VALUE_REQUIRED, 'Env file');
+
+    }
+
     /**
      * @param InputInterface  $input
      * @param OutputInterface $output
@@ -55,6 +66,9 @@ class GenerateJwtKeysCommand extends Command
      */
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
+
+        $variablePrefix = $input->getArgument('variable-prefix');
+        $envFile = $input->getOption('env-file') ?? '.env';
 
         $io = new SymfonyStyle($input, $output);
         $editor = new DotenvEditor();
@@ -68,16 +82,43 @@ class GenerateJwtKeysCommand extends Command
         $publicKey = base64_encode(file_get_contents($publicPath));
         $privateKey = base64_encode(file_get_contents($privatePath));
 
-        $editor->load('.env');
-        $editor->set('JWT_PUBLIC_KEY', $publicKey);
-        $editor->set('JWT_PRIVATE_KEY', $privateKey);
+        unlink($publicPath);
+        unlink($privatePath);
 
-        $editor->save('.env');
+        $editor->load($envFile);
+        $editor->set($this->getFullPublic($variablePrefix), $publicKey);
+        $editor->set($this->getFullPrivate($variablePrefix), $privateKey);
 
-        $io->success('New JWT_PUBLIC_KEY was generated: ' . $publicKey);
-        $io->success('New JWT_PRIVATE_KEY was generated: ' . $privateKey);
+        $editor->save($envFile);
+
+        $io->success("New {$this->getFullPublic($variablePrefix)} was generated: $publicKey");
+        $io->success("New {$this->getFullPrivate($variablePrefix)} was generated: $privateKey");
 
         return self::SUCCESS;
+
+    }
+
+    /**
+     * @param string $prefix
+     *
+     * @return string
+     */
+    private function getFullPublic(string $prefix): string
+    {
+
+        return $prefix . '_PUBLIC_KEY';
+
+    }
+
+    /**
+     * @param string $prefix
+     *
+     * @return string
+     */
+    private function getFullPrivate(string $prefix): string
+    {
+
+        return $prefix . '_PRIVATE_KEY';
 
     }
 
