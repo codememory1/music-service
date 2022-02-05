fee412515f9a912a3750baefcf775e9041cba4f9
Danil <kostynd1@gmail.com>
2022-02-05 02:05:21 +0200

    New register files

---
+++ app/src/Service/Security/Register/CreatorActivationTokenService.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Service\Security\Register;
+
+/**
+ * Class CreatorActivationTokenService
+ *
+ * @package App\Service\Security\Register
+ *
+ * @author  codememory
+ */
+class CreatorActivationTokenService
+{
+
+}
---
+++ app/src/Service/Security/Register/UserRegisterService.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Service\Security\Register;
+
+use App\Service\AbstractApiService;
+use App\Service\Response\ApiResponseService;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+
+/**
+ * Class UserRegisterService
+ *
+ * @package App\Service\Security\Register
+ *
+ * @author  codememory
+ */
+class UserRegisterService extends AbstractApiService
+{
+
+    public function register(ValidatorInterface $validator): ApiResponseService
+    {
+
+
+
+    }
+
+}
