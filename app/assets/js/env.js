7b329c1ad3d6aef0cae727fe98d0baed5c4bf6dd
Danil <kostynd1@gmail.com>
2022-02-06 03:19:02 +0200

    Изменение env окружений

--- app/assets/js/env.js
+++ app/assets/js/env.js
@@ -1,7 +1,6 @@
-const APP_NAME = "Music Service";
-const APP_URL = process.env.MIX_APP_URL;
-const PLAYER_URL = APP_URL + '/web-player';
-const API_URL = APP_URL + '/api/v1';
-const AUTH_COOKIE_NAME = "access_token";
-
-export { APP_NAME, APP_URL, PLAYER_URL, API_URL, AUTH_COOKIE_NAME };
+export const APP_NAME = "Music Service";
+export const APP_DOMAIN = process.env.MIX_APP_DOMAIN;
+export const APP_URL = process.env.MIX_APP_URL;
+export const PLAYER_URL = APP_URL + "/web-player";
+export const API_URL = APP_URL + "/api/v1";
+export const AUTH_COOKIE_NAME = "access_token";
