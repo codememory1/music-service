a64ee77fc27b44237a01503b59e62c34eb5bb90e
Danil <kostynd1@gmail.com>
2022-02-06 03:25:59 +0200

    Исправлен ключ username

--- app/assets/js/api/auth.js
+++ app/assets/js/api/auth.js
@@ -2,7 +2,7 @@ import BaseAxios from "../modules/BaseAxios";
 
 async function response(username, password) {
   try {
-    return await BaseAxios.post("/auth", { username, password });
+    return await BaseAxios.post("/user/auth", { login: username, password });
   } catch (e) {
     return e.response;
   }
