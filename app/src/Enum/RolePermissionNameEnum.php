bf54b623c5619bae2c7e6f7f3180e76c87dd9737
Danil <kostynd1@gmail.com>
2022-02-11 12:11:42 +0200

    Fix under 7c4a423

--- app/src/Enum/RolePermissionNameEnum.php
+++ app/src/Enum/RolePermissionNameEnum.php
@@ -12,25 +12,25 @@ namespace App\Enum;
 enum RolePermissionNameEnum: string
 {
 
-    public const ADD_TRACK = 'add-music';
-    public const UPDATE_TRACK = 'update-music';
-    public const DELETE_TRACK = 'delete-music';
+    case ADD_TRACK = 'add-music';
+    case UPDATE_TRACK = 'update-music';
+    case DELETE_TRACK = 'delete-music';
 
-    public const CREATE_SUBSCRIPTION = 'create-subscription';
-    public const UPDATE_SUBSCRIPTION = 'update-subscription';
-    public const DELETE_SUBSCRIPTION = 'delete-subscription';
+    case CREATE_SUBSCRIPTION = 'create-subscription';
+    case UPDATE_SUBSCRIPTION = 'update-subscription';
+    case DELETE_SUBSCRIPTION = 'delete-subscription';
 
-    public const CREATE_LANG = 'create-language';
-    public const UPDATE_LANG = 'update-language';
-    public const DELETE_LANG = 'delete-language';
+    case CREATE_LANG = 'create-language';
+    case UPDATE_LANG = 'update-language';
+    case DELETE_LANG = 'delete-language';
 
-    public const CREATE_TRANSLATION_KEY = 'create-translation-key';
-    public const UPDATE_TRANSLATION_KEY = 'update-translation-key';
-    public const DELETE_TRANSLATION_KEY = 'delete-translation-key';
+    case CREATE_TRANSLATION_KEY = 'create-translation-key';
+    case UPDATE_TRANSLATION_KEY = 'update-translation-key';
+    case DELETE_TRANSLATION_KEY = 'delete-translation-key';
 
-    public const CREATE_TRANSLATION = 'create-translation-to-language';
-    public const UPDATE_TRANSLATION = 'update-language-translation';
-    public const DELETE_TRANSLATION = 'delete-translation-from-language';
+    case CREATE_TRANSLATION = 'create-translation-to-language';
+    case UPDATE_TRANSLATION = 'update-language-translation';
+    case DELETE_TRANSLATION = 'delete-translation-from-language';
 
     /**
      * @return array
