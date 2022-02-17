2f2651353b1dbfbbf971155c1e2641ebd3aa67f8
Danil <kostynd1@gmail.com>
2022-02-17 03:13:29 +0200

    Доработки инсталлера

--- installer.sh
+++ installer.sh
@@ -1,97 +1,117 @@
 #!/bin/bash
 
 function join {
-  local IFS="$1";
-  shift;
+  local IFS="$1"
+  shift
 
-  echo "$*";
+  echo "$*"
 }
 
 # Определить окружение
-function environment (){
-  available=("dev" "prod");
-  fined=false;
+function environment() {
+  available=("dev" "prod")
+  fined=false
 
-  echo -n -e "\e[32mВыбирите окружение(\e[1;33m$(join , ${available[@]})\e[0m): \e[0m";
-  read selected_env;
+  echo -n -e "\e[32m Выбирите окружение(\e[1;33m$(join , ${available[@]})\e[32m): \e[0m"
+  read selected_env
 
   # Проверить существование окружения
   for env in ${available[@]}; do
     if [[ "$env" == "$selected_env" ]]; then
-      fined=true;
-      break;
+      fined=true
+      break
     fi
   done
 
   # Определить корректнсоть выбранного окружения
   if [[ "$fined" == false ]]; then
-    echo -e "\tError: Некорректное окружение\n";
-    environment;
+    echo -e "\tError: Некорректное окружение\n"
+    environment
   else
-    echo -e "\e[36m\n[OK] Выбранно окружение=${selected_env}\n\e[0m"
+    echo -e "\e[36m\n [OK] Выбранно окружение=${selected_env}\n\e[0m"
   fi
 }
-environment;
+environment
 
 # Настройка MYSQL
-function mysqlSettings (){
-  username="";
-  password="";
-  dbname="";
+function mysqlSettings() {
+  username=""
+  password=""
+  dbname=""
 
   echo -n -e "\n
-╔―――――――――――――――――――――――――╗
-|     Настройка MYSQL     │
-╚―――――――――――――――――――――――――╝
-\n";
+    ╔―――――――――――――――――――――――――╗
+    |     Настройка MYSQL     │
+    ╚―――――――――――――――――――――――――╝
+  \n"
 
   # Вопрос о имени пользователя
-  mysqlUser (){
-      echo -n -e "\e[32mУкажите имя пользователя: \e[0m";
-      read username;
+  mysqlUser() {
+    echo -n -e "\e[32m Укажите имя пользователя: \e[0m"
+    read username
 
-      if [[ -z $username ]]; then
-        echo -e "\tError: Укажите имя пользователя\n";
-        mysqlUser;
-      fi
+    if [[ -z $username ]]; then
+      echo -e "\tError: Укажите имя пользователя\n"
+      mysqlUser
+    fi
   }
-  mysqlUser;
+  mysqlUser
 
   # Вопрос о пароле пользователя
-  mysqlPassword (){
-    echo -n -e "\e[32mУкажите пароль пользователя: \e[0m";
-    read password;
+  mysqlPassword() {
+    echo -n -e "\e[32m Укажите пароль пользователя: \e[0m"
+    read password
 
     if [[ -z $password ]]; then
-      echo -e "\tError: Укажите пароль пользователя\n";
-      mysqlPassword;
+      echo -e "\tError: Укажите пароль пользователя\n"
+      mysqlPassword
     fi
   }
-  mysqlPassword;
+  mysqlPassword
 
   # Вопрос о имени базы данных
-  mysqlDbname (){
-    echo -n -e "\e[32mУкажите имя базы данных: \e[0m";
-    read dbname;
+  mysqlDbname() {
+    echo -n -e "\e[32m Укажите имя базы данных: \e[0m"
+    read dbname
 
     if [[ -z $dbname ]]; then
-      echo -e "\tError: Укажите имя базы данных\n";
-      mysqlDbname;
+      echo -e "\tError: Укажите имя базы данных\n"
+      mysqlDbname
     fi
   }
-  mysqlDbname;
+  mysqlDbname
 
-  echo -e "\e[36m\n[OK] Настройка MYSQL завершена\n\e[0m"
+  echo -e "\e[36m\n [OK] Настройка MYSQL завершена\n\e[0m"
 
   echo -e "
-Созданная вами конфигурация MYSQL:
+ Созданная вами конфигурация MYSQL:
 
-  - \e[1;33mMYSQL_USER\e[0m     ${username}
-  - \e[1;33mMYSQL_PASSWORD\e[0m ${password}
-  - \e[1;33mMYSQL_DBNAME\e[0m   ${dbname}
-  ";
+    - \e[1;33mMYSQL_USER\e[0m     ${username}
+    - \e[1;33mMYSQL_PASSWORD\e[0m ${password}
+    - \e[1;33mMYSQL_DBNAME\e[0m   ${dbname}
+  "
 }
 mysqlSettings
 
+# Сгенерировать JWT ключи, для авторизации
+function generateJWT() {
+  echo -n -e "\e[32m Хотите сгенерировать jwt ключи? (\e[1;33mY/n\e[32m): \e[0m"
+  read generateJWT
+  isGenerateKeys=false
+
+  case "$generateJWT" in
+  "y" | "Y")
+    isGenerateKeys=true
+    ;;
+  "n" | "N")
+    isGenerateKeys=false
+    ;;
+  *)
+    echo -e "\tError: Укажите корректный ответ\n"
+    generateJWT
+    ;;
+  esac
+}
+generateJWT
 
 
