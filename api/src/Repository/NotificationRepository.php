6fb773d2361617f05a1c24644abdcfba0d1b1155
Данил <kostynd1@gmail.com>
2022-06-07 01:59:39 +0300

    MS-13 Исправлен репозиторий уведомлений

--- api/src/Repository/NotificationRepository.php
+++ api/src/Repository/NotificationRepository.php
@@ -3,64 +3,19 @@
 namespace App\Repository;
 
 use App\Entity\Notification;
-use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Doctrine\Persistence\ManagerRegistry;
 
 /**
- * @extends ServiceEntityRepository<Notification>
+ * Class NotificationRepository.
  *
- * @method null|Notification find($id, $lockMode = null, $lockVersion = null)
- * @method null|Notification findOneBy(array $criteria, array $orderBy = null)
- * @method Notification[]    findAll()
- * @method Notification[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ * @package App\Repository
+ * @template-extends AbstractRepository<Notification>
+ *
+ * @author  Codememory
  */
-class NotificationRepository extends ServiceEntityRepository
+class NotificationRepository extends AbstractRepository
 {
-    public function __construct(ManagerRegistry $registry)
-    {
-        parent::__construct($registry, Notification::class);
-    }
-
-    public function add(Notification $entity, bool $flush = false): void
-    {
-        $this->getEntityManager()->persist($entity);
-
-        if ($flush) {
-            $this->getEntityManager()->flush();
-        }
-    }
-
-    public function remove(Notification $entity, bool $flush = false): void
-    {
-        $this->getEntityManager()->remove($entity);
-
-        if ($flush) {
-            $this->getEntityManager()->flush();
-        }
-    }
-
-//    /**
-//     * @return Notification[] Returns an array of Notification objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('n')
-//            ->andWhere('n.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('n.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
-
-//    public function findOneBySomeField($value): ?Notification
-//    {
-//        return $this->createQueryBuilder('n')
-//            ->andWhere('n.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
+    /**
+     * @inheritDoc
+     */
+    protected ?string $entity = Notification::class;
 }
