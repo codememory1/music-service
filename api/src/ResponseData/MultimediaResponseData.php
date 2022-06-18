b45ce607c06a73f9ecbdb3562184dc1bef00b92c
Данил <kostynd1@gmail.com>
2022-06-19 02:30:10 +0300

    MS-3 Удаление библиотеки для получения metadata

---
+++ api/src/ResponseData/MultimediaResponseData.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\ResponseData;
+
+use App\ResponseData\Interfaces\ResponseDataInterface;
+
+/**
+ * Class MultimediaResponseData
+ *
+ * @package App\ResponseData
+ *
+ * @author  Codememory
+ */
+class MultimediaResponseData extends AbstractResponseData implements ResponseDataInterface
+{
+    
+}
