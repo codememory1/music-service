ae43cb54af241334adefd0b1c29ea4b555f8d28d
Данил <kostynd1@gmail.com>
2022-06-25 00:39:12 +0300

    MS-130 Сохранение изображения альбома через слушатель

--- api/src/Service/Album/SaveAlbumService.php
+++ api/src/Service/Album/SaveAlbumService.php
@@ -2,6 +2,14 @@
 
 namespace App\Service\Album;
 
+use App\DTO\AlbumDTO;
+use App\Entity\Album;
+use App\Enum\EventEnum;
+use App\Event\SaveAlbumEvent;
+use App\Service\AbstractService;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use Symfony\Contracts\Service\Attribute\Required;
+
 /**
  * Class SaveAlbumService.
  *
@@ -9,9 +17,28 @@ namespace App\Service\Album;
  *
  * @author  Codememory
  */
-class SaveAlbumService
+class SaveAlbumService extends AbstractService
 {
-    public function make(): void
+    #[Required]
+    public ?EventDispatcherInterface $eventDispatcher = null;
+
+    /**
+     * @param AlbumDTO $albumDTO
+     * @param Album    $album
+     *
+     * @return void
+     */
+    public function make(AlbumDTO $albumDTO, Album $album): void
     {
+        if (null === $album->getId()) {
+            $this->em->persist($album);
+        }
+
+        $this->em->flush();
+
+        $this->eventDispatcher->dispatch(
+            new SaveAlbumEvent($albumDTO, $album),
+            EventEnum::SAVE_ALBUM->value
+        );
     }
 }
