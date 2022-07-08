4df6223f623f51051c8ea3132ad824d649a3564c
Данил <kostynd1@gmail.com>
2022-07-09 02:34:31 +0300

    MS-167 Созданы фильтры и сортировки плейлистов и не большие правки

--- api/src/Repository/PlaylistRepository.php
+++ api/src/Repository/PlaylistRepository.php
@@ -18,6 +18,30 @@ class PlaylistRepository extends AbstractRepository
     protected ?string $entity = Playlist::class;
     protected ?string $alias = 'p';
 
+    protected function findByCriteria(array $criteria, array $orderBy = []): array
+    {
+        $this->qb->leftJoin('p.multimedia', 'm');
+
+        if (false !== $sortByTitle = $this->sortService->get('title')) {
+            $orderBy['p.title'] = $this->getOrderType($sortByTitle);
+        }
+
+        if (false !== $sortByCreatedAt = $this->sortService->get('createdBy')) {
+            $orderBy['p.createdBy'] = $this->getOrderType($sortByCreatedAt);
+        }
+
+        if (false !== $sortByNumberMultimedia = $this->sortService->get('numberMultimedia')) {
+            $orderBy['COUNT(m.id)'] = $this->getOrderType($sortByNumberMultimedia);
+        }
+
+        if (false !== $filterByTitle = $this->filterService->get('title')) {
+            $this->qb->andWhere($this->qb->expr()->like('tk.key', ':key'));
+            $this->qb->setParameter('key', "${$filterByTitle}@%");
+        }
+
+        return parent::findByCriteria($criteria, $orderBy);
+    }
+
     public function findByUser(User $user): array
     {
         return $this->findByCriteria([
