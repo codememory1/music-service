9107cc30c402ed7ec6b9d015a2fd841d36d1189d
Данил <kostynd1@gmail.com>
2022-07-09 20:54:55 +0300

    MS-170 Запрет добавления повторной мультимедиа в директорию плейлиста

--- api/src/Service/PlaylistDirectory/AddMultimediaToPlaylistDirectoryService.php
+++ api/src/Service/PlaylistDirectory/AddMultimediaToPlaylistDirectoryService.php
@@ -5,8 +5,11 @@ namespace App\Service\PlaylistDirectory;
 use App\Entity\MultimediaMediaLibrary;
 use App\Entity\MultimediaPlaylistDirectory;
 use App\Entity\PlaylistDirectory;
+use App\Repository\MultimediaPlaylistDirectoryRepository;
+use App\Rest\Http\Exceptions\EntityExistException;
 use App\Service\AbstractService;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Contracts\Service\Attribute\Required;
 
 /**
  * Class AddMultimediaToPlaylistDirectoryService.
@@ -17,8 +20,19 @@ use Symfony\Component\HttpFoundation\JsonResponse;
  */
 class AddMultimediaToPlaylistDirectoryService extends AbstractService
 {
+    #[Required]
+    public ?MultimediaPlaylistDirectoryRepository $multimediaPlaylistDirectoryRepository = null;
+
     public function make(PlaylistDirectory $playlistDirectory, MultimediaMediaLibrary $multimediaMediaLibrary): JsonResponse
     {
+        $multimediaPlaylistDirectory = $this->multimediaPlaylistDirectoryRepository->findOneBy([
+            'multimediaMediaLibrary' => $multimediaMediaLibrary
+        ]);
+
+        if (null !== $multimediaPlaylistDirectory) {
+            throw EntityExistException::multimediaPlaylistDirectory();
+        }
+
         $multimediaPlaylistDirectory = new MultimediaPlaylistDirectory();
 
         $multimediaPlaylistDirectory->setMultimediaMediaLibrary($multimediaMediaLibrary);
