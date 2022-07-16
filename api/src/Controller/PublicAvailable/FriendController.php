6fb962ff5ef40da85f7e8656c3f074e55b59d099
Данил <kostynd1@gmail.com>
2022-07-16 03:07:45 +0300

    MS-180 Запрос на одобрение заявки на дружбу

--- api/src/Controller/PublicAvailable/FriendController.php
+++ api/src/Controller/PublicAvailable/FriendController.php
@@ -5,10 +5,13 @@ namespace App\Controller\PublicAvailable;
 use App\Annotation\Authorization;
 use App\Annotation\EntityNotFound;
 use App\Annotation\SubscriptionPermission;
+use App\Entity\Friend;
 use App\Entity\User;
+use App\Enum\FriendStatusEnum;
 use App\Enum\SubscriptionPermissionEnum;
 use App\Rest\Controller\AbstractRestController;
 use App\Rest\Http\Exceptions\EntityNotFoundException;
+use App\Service\Friend\AcceptAsFriendService;
 use App\Service\Friend\AddAsFriendService;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
@@ -31,5 +34,20 @@ class FriendController extends AbstractRestController
         AddAsFriendService $addAsFriendService
     ): JsonResponse {
         return $addAsFriendService->make($this->authorizedUser->getUser(), $friend);
+    }
+
+    #[Route('/friend/{friend_id<\d+>}/accept', methods: 'PATCH')]
+    #[Authorization]
+    #[SubscriptionPermission(SubscriptionPermissionEnum::ADD_AS_FRIEND)]
+    public function acceptAsFriend(
+        #[EntityNotFound(EntityNotFoundException::class, 'friend')] Friend $friend,
+        AcceptAsFriendService $acceptAsFriendService
+    ): JsonResponse {
+        if ($friend->getFriend()->getId() !== $this->authorizedUser->getUser()->getId()
+            || $friend->getStatus() !== FriendStatusEnum::AWAITING_CONFIRMATION->name) {
+            throw EntityNotFoundException::friend();
+        }
+
+        return $acceptAsFriendService->make($friend);
     }
 }
