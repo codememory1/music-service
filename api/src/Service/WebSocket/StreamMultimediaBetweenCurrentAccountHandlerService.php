5c34fac621c3233932b4b9830c541c1541f3bfe1
Данил <kostynd1@gmail.com>
2022-08-18 01:58:52 +0300

    MS-202 Правки по merge dev

--- api/src/Service/WebSocket/StreamMultimediaBetweenCurrentAccountHandlerService.php
+++ api/src/Service/WebSocket/StreamMultimediaBetweenCurrentAccountHandlerService.php
@@ -7,51 +7,14 @@ use App\Entity\StreamRunningMultimedia;
 use App\Entity\UserSession;
 use App\Enum\StreamMultimediaStatusEnum;
 use App\Enum\WebSocketClientMessageTypeEnum;
+use App\Exception\WebSocket\EntityNotFoundException;
 
-/**
- * Class StreamMultimediaBetweenCurrentAccountHandlerService.
- *
- * @package App\Service\WebSocket
- *
- * @author  Codememory
- */
 final class StreamMultimediaBetweenCurrentAccountHandlerService extends AbstractUserMessageHandlerService
 {
     protected ?WebSocketClientMessageTypeEnum $clientMessageType = WebSocketClientMessageTypeEnum::STREAM_MULTIMEDIA_BETWEEN_CURRENT_ACCOUNT;
 
     public function handler(): void
     {
-        $runningMultimedia = $this->getEntityIfExist('running_multimedia_id', RunningMultimedia::class);
-        $toUserSession = $this->getEntityIfExist('to_session_id', UserSession::class);
-
-        $this->worker->sendToSession($toUserSession, WebSocketClientMessageTypeEnum::MULTIMEDIA_BROADCAST_REQUEST, [
-            'to_user_session' => 'Session is not active'
-        ]);
-
-        if (null !== $runningMultimedia) {
-            $toUserSession = $this->getEntityIfExist('to_session_id', UserSession::class);
-
-            if (null !== $toUserSession) {
-                if (false === $toUserSession->isActive()) {
-//                    $this->worker->sendToConnection($this->getConnection(), WebSocketClientMessageTypeEnum::MULTIMEDIA_BROADCAST_REQUEST, [
-//                        'to_user_session' => 'Session is not active'
-//                    ]);
-                } else {
-                    echo $toUserSession . "\n";
-                    $this->worker->sendToUser($toUserSession->getUser(), WebSocketClientMessageTypeEnum::MULTIMEDIA_BROADCAST_REQUEST, [
-                        'to_user_session' => 'Session is not active'
-                    ]);
-                    $streamRunningMultimedia = new StreamRunningMultimedia();
-
-                    $streamRunningMultimedia->setFromUserSession($this->authorizedUser->getUserSession());
-                    $streamRunningMultimedia->setToUserSession($toUserSession);
-                    $streamRunningMultimedia->setRunningMultimedia($runningMultimedia);
-                    $streamRunningMultimedia->setStatus(StreamMultimediaStatusEnum::PENDING);
-
-                    $this->em->persist($streamRunningMultimedia);
-                    $this->em->flush();
-                }
-            }
-        }
+        throw EntityNotFoundException::userSession(WebSocketClientMessageTypeEnum::MULTIMEDIA_BROADCAST_REQUEST);
     }
 }
