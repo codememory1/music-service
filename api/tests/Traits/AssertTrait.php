fa8e471a1606e523c1b91f40990d547d5567cdc6
Данил <kostynd1@gmail.com>
2022-08-31 03:26:59 +0300

    MS-35 Правки по ассертам

--- api/tests/Traits/AssertTrait.php
+++ api/tests/Traits/AssertTrait.php
@@ -10,22 +10,22 @@ trait AssertTrait
 {
     protected function assertApiStatusCode(int $expect, ?string $message = null): void
     {
-        $this->assertEquals($expect, $this->response['status_code'], $message ?? '');
+        $this->assertEquals($expect, $this->response['status_code'], $message ?: '');
     }
 
     protected function assertApiType(ResponseTypeEnum $expect, ?string $message = null): void
     {
-        $this->assertEquals($expect->name, $this->response['type'], $message ?? '');
+        $this->assertEquals($expect->name, $this->response['type'], $message ?: '');
     }
 
     protected function assertApiMessage(string|array $expect, ?string $message = null): void
     {
-        $this->assertEquals($expect, $this->response['message'], $message ?? '');
+        $this->assertEquals($expect, $this->response['message'], $message ?: '');
     }
 
     protected function assertApiData(array $expect, ?string $message = null): void
     {
-        $this->assertEquals($expect, $this->response['data'], $message ?? '');
+        $this->assertEquals($expect, $this->response['data'], $message ?: '');
     }
 
     protected function assertIsType(string|array $expect, mixed $value, ?string $message = null): void
@@ -46,23 +46,23 @@ trait AssertTrait
         $this->assertTrue($isType, $message ?: "Failed to validate that the value is one of the \"${expectTypesToString}\" types.");
     }
 
-    protected function assertDateTime(mixed $actual, string $message = ''): void
+    protected function assertDateTime(mixed $actual, ?string $message = null): void
     {
         $this->assertIsString($actual);
         $this->assertMatchesRegularExpression(
             '/^[0-9]{4}-[0-9]{01,12}-[0-9]{01,31}\s[0-9]{00,24}:[0-9]{00,24}:[0-9]{00,60}$/',
             $actual,
-            $message
+            $message ?: ''
         );
     }
 
-    protected function assertDateTimeWithNull(mixed $actual, string $message = ''): void
+    protected function assertDateTimeWithNull(mixed $actual, ?string $message = null): void
     {
         $this->assertIsString($actual ?: '');
         $this->assertMatchesRegularExpression(
             '/^([0-9]{4}-[0-9]{01,12}-[0-9]{01,31}\s[0-9]{00,24}:[0-9]{00,24}:[0-9]{00,60})|(\s*)$/',
             $actual ?: '',
-            $message
+            $message ?: ''
         );
     }
 
