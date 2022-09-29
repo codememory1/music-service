af8a401d5ef833b44ef5257240c3689506c494bb
Данил <kostynd1@gmail.com>
2022-09-30 02:14:57 +0300

    MS-160 Правки по коду

--- api/src/Repository/MultimediaStatisticRepository.php
+++ api/src/Repository/MultimediaStatisticRepository.php
@@ -3,64 +3,12 @@
 namespace App\Repository;
 
 use App\Entity\MultimediaStatistic;
-use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Doctrine\Persistence\ManagerRegistry;
 
 /**
- * @extends ServiceEntityRepository<MultimediaStatistic>
- *
- * @method null|MultimediaStatistic find($id, $lockMode = null, $lockVersion = null)
- * @method null|MultimediaStatistic findOneBy(array $criteria, array $orderBy = null)
- * @method MultimediaStatistic[]    findAll()
- * @method MultimediaStatistic[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ * @template-extends AbstractRepository<MultimediaStatistic>
  */
-class MultimediaStatisticRepository extends ServiceEntityRepository
+final class MultimediaStatisticRepository extends AbstractRepository
 {
-    public function __construct(ManagerRegistry $registry)
-    {
-        parent::__construct($registry, MultimediaStatistic::class);
-    }
-
-    public function add(MultimediaStatistic $entity, bool $flush = false): void
-    {
-        $this->getEntityManager()->persist($entity);
-
-        if ($flush) {
-            $this->getEntityManager()->flush();
-        }
-    }
-
-    public function remove(MultimediaStatistic $entity, bool $flush = false): void
-    {
-        $this->getEntityManager()->remove($entity);
-
-        if ($flush) {
-            $this->getEntityManager()->flush();
-        }
-    }
-
-//    /**
-//     * @return MultimediaStatistic[] Returns an array of MultimediaStatistic objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('m')
-//            ->andWhere('m.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('m.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
-
-//    public function findOneBySomeField($value): ?MultimediaStatistic
-//    {
-//        return $this->createQueryBuilder('m')
-//            ->andWhere('m.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
+    protected ?string $entity = MultimediaStatistic::class;
+    protected ?string $alias = 'ms';
 }
