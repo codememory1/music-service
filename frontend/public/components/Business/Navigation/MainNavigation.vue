094aa71865ec07940573b18c7e8e45db861ff1f3
Данил <kostynd1@gmail.com>
2022-10-04 00:25:53 +0300

    MS-231 Создана основная навигация и удалены ресурсы из корня

---
+++ frontend/public/components/Business/Navigation/MainNavigation.vue
@@ -0,0 +1,31 @@
+<template>
+  <nav class="main-navigation">
+    <ul class="main-navigation-items">
+      <li class="main-navigation__item">
+        <NuxtLink to="" class="main-navigation__link">Premium</NuxtLink>
+      </li>
+      <li class="main-navigation__item">
+        <NuxtLink to="" class="main-navigation__link">Help</NuxtLink>
+      </li>
+      <li class="main-navigation__item">
+        <NuxtLink to="" class="main-navigation__link">Donwload</NuxtLink>
+      </li>
+      <li class="main-navigation__item main-navigation__item--sign-up">
+        <NuxtLink to="" class="main-navigation__link">Sign up</NuxtLink>
+      </li>
+      <li class="main-navigation__item">
+        <NuxtLink to="" class="main-navigation__link">Sign in</NuxtLink>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+export default class MainNavigation extends Vue {}
+</script>
+
+<style lang="scss">
+@import '@/assets/scss/business/navigation/main-navigation';
+</style>
