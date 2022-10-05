850144b3ac4fcff0e916be4dbf5fb86247f34523
Данил <kostynd1@gmail.com>
2022-10-06 01:51:41 +0300

    MS-96 Добавлен switchbox компонент и доработки по другим компонентам

--- frontend/public/components/UI/Checkbox/BaseCheckbox.vue
+++ frontend/public/components/UI/Checkbox/BaseCheckbox.vue
@@ -1,12 +1,15 @@
 <template>
-  <input v-model="checked" type="checkbox" class="checkbox" />
+  <input v-model="checked" type="checkbox" class="checkbox" :value="inputValue" />
 </template>
 
 <script lang="ts">
-import { Component, Vue, VModel } from 'vue-property-decorator';
+import { Component, Vue, VModel, Prop } from 'vue-property-decorator';
 
 @Component
 export default class BaseCheckbox extends Vue {
+  @Prop({ required: false, default: false })
+  private readonly inputValue!: string | number | boolean;
+
   @VModel({ required: false, default: false })
   private checked!: string | number | boolean;
 }
--- frontend/public/components/UI/Radiobox/BaseRadiobox.vue
+++ frontend/public/components/UI/Radiobox/BaseRadiobox.vue
@@ -1,12 +1,15 @@
 <template>
-  <input v-model="checked" type="radio" class="radiobox" />
+  <input v-model="checked" type="radio" class="radiobox" :value="inputValue" />
 </template>
 
 <script lang="ts">
-import { Component, Vue, VModel } from 'vue-property-decorator';
+import { Component, Vue, Prop, VModel } from 'vue-property-decorator';
 
 @Component
 export default class BaseRadiobox extends Vue {
+  @Prop({ required: false })
+  private readonly inputValue!: string | number | boolean;
+
   @VModel({ required: false, default: null })
   private checked!: any;
 }
