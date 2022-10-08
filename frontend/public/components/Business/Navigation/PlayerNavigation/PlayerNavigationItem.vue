4797d50949be988168c8804415f74b22bac2d7d5
Данил <kostynd1@gmail.com>
2022-10-08 19:19:59 +0300

    MS-105 Создан компонент навигации в плеере

---
+++ frontend/public/components/Business/Navigation/PlayerNavigation/PlayerNavigationItem.vue
@@ -0,0 +1,28 @@
+<template>
+  <li class="player-navigation__item">
+    <NuxtLink class="player-navigation__item-link" :to="to">
+      <i v-if="faIcon !== undefined" class="player-navigation__item-icon" :class="faIcon" />
+      {{ title }}
+    </NuxtLink>
+  </li>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'vue-property-decorator';
+
+@Component
+export default class PlayerNavigationItem extends Vue {
+  @Prop({ required: false })
+  private readonly faIcon!: string;
+
+  @Prop({ required: true })
+  private readonly to!: string;
+
+  @Prop({ required: true })
+  private readonly title!: string;
+}
+</script>
+
+<style lang="scss">
+@import '@/assets/scss/business/player-navigation/player-navigation-item';
+</style>
