7104c8775e065b7f78cab747c3d23a5484ad8029
Данил <kostynd1@gmail.com>
2022-10-08 19:35:06 +0300

    MS-109 Создан компонент SimilarArtist для отоброжения в похожих артистах

---
+++ frontend/public/components/Business/Artist/SimilarArtist.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="similar-artists">
+    <div class="similar-artist">
+      <NuxtLink to="/">
+        <img :src="image" :alt="name" />
+      </NuxtLink>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'vue-property-decorator';
+
+@Component
+export default class SimilarArtist extends Vue {
+  @Prop({ required: true })
+  private readonly id!: number;
+
+  @Prop({ required: true })
+  private readonly name!: string;
+
+  @Prop({ required: true })
+  private readonly image!: string;
+}
+</script>
+
+<style lang="scss">
+@import '@/assets/scss/business/artist/similar-artist';
+</style>
