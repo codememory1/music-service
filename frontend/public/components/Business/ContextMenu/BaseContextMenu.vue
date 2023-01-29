b116f1cda5803c9e3328c4ab62adbdb481f12af6
Данил <kostynd1@gmail.com>
2023-01-29 07:12:46 +0300

    MS-292 Создано контекстное меню и исправление стилей

---
+++ frontend/public/components/Business/ContextMenu/BaseContextMenu.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="context-menu">
+    <div class="context-menu-groups">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class BaseContextMenu extends Vue {}
+</script>
+
+<style lang="scss">
+@import '@/assets/scss/components/business/context-menu/base-context-menu.scss';
+</style>
